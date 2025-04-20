08e47f17e4cc649bd276d176f2ca1c04333ada5e
Руслан Галяудинов <rus4855@yandex.ru>
2025-04-21 00:01:35 +0300

    homework 10.1 done

--- src/processing.py
+++ src/processing.py
@@ -1,19 +1,16 @@
 from typing import Any
 
-def filter_by_state(list_of_dicts: list[dict[str, Any]], state: str = 'EXECUTED') -> list[dict[str, Any]]:
+
+def filter_by_state(list_of_dicts: list[dict[str, Any]], state: str = "EXECUTED") -> list[dict[str, Any]]:
     """Функция возвращает новый список словарей, содержащий только те словари, у которых ключ
-state соответствует указанному значению"""
+    state соответствует указанному значению"""
 
     filtered_list = [i for i in list_of_dicts if i.get("state") == state]
 
     return filtered_list
 
 
-def sort_by_date(list_for_sorted_by_date: list[dict[str, Any]], ascending: bool=True) -> list[dict[str, Any]]:
+def sort_by_date(list_for_sorted_by_date: list[dict[str, Any]], ascending: bool = True) -> list[dict[str, Any]]:
     """Функция возвращает новый список, отсортированный по дате"""
 
-    return sorted(list_for_sorted_by_date, key=lambda x: x.get('date'), reverse=ascending)
-
-
-
-
+    return sorted(list_for_sorted_by_date, key=lambda x: x.get("date"), reverse=ascending)
