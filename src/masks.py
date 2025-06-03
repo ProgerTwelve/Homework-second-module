f37bfb0f0a1cb680290888f1f0812b70daa5b93f
Руслан Галяудинов <rus4855@yandex.ru>
2025-06-03 15:24:03 +0300

    Создано логирование для модулей utils.py и masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,28 +1,53 @@
+import logging
 from typing import Union
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    filename="../logs/masks.log",  # Запись логов в папку logs в файл masks.log
+    encoding="utf-8",
+    filemode="w",
+)  # Перезапись файла при каждом запуске
+
+masks_log = logging.getLogger("masks")
 
 
 def get_mask_card_number(card_number: Union[int, str]) -> str:
     """Маскирует номер банковской карты в формате 0000 00** **** 0000"""
 
+    masks_log.info(f"Проверка {card_number} на соответствие типу int или str ")
     if isinstance(card_number, int) or isinstance(card_number, str):
+        masks_log.info("Проверка успешно пройдена")
         str_number = str(card_number)
+        masks_log.info("Проверка длины карты")
         if len(str_number) == 16:
+            masks_log.info("Проверка успешно пройдена")
             str_card_number = str_number[0:4] + " " + str_number[4:6] + "** **** " + str_number[-4:]
+            masks_log.info(f"Возврат значения {str_card_number}")
             return str_card_number
         else:
+            masks_log.error("Проверка провалена. Длина номера карты не 16 чисел")
             raise ValueError("Неверный формат данных!")
     else:
+        masks_log.error(f"Проверка провалена. Тип данных {card_number} не является целым числом либо строкой")
         raise TypeError("Неверный тип данных!")
 
 
 def get_mask_account(account: Union[int, str]) -> str:
     """Маскирует номер банковского счета в формате **0000"""
 
+    masks_log.info(f"Проверка {account} на соответствие типу int или str ")
     if isinstance(account, int) or isinstance(account, str):
+        masks_log.info("Проверка успешно пройдена")
         str_account = str(account)
+        masks_log.info("Проверка длины символов банковского счета")
         if len(str_account) == 20:
+            masks_log.info("Проверка успешно пройдена")
+            masks_log.info("Функция успешно вернула замаскированный счет")
             return "**" + str_account[-4:]
         else:
+            masks_log.error(f"Проверка {str_account} на длину символов не пройдена ")
             raise ValueError("Неверный формат данных!")
     else:
+        masks_log.error(f"Ошибка! {account} не является типом int или str")
         raise TypeError("Неверный тип данных!")
--- src/utils.py
+++ src/utils.py
@@ -1,17 +1,33 @@
 import json
+import logging
 from typing import Any
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    filename="../logs/utils.log",  # Запись логов в папку logs в файл utils.log
+    encoding="utf-8",
+    filemode="w",
+)  # Перезапись файла при каждом запуске
+
+utils_log = logging.getLogger("utils")
 
 
 def get_data_from_json_file(path: str) -> list[dict[Any, Any]]:
     """Принимает на вход путь до JSON-файла и возвращает список словарей
     с данными о финансовых транзакциях."""
     try:
+        utils_log.info(f"Открытие файла {path}")
         with open(path, encoding="utf-8") as json_data:
+            utils_log.info(f"Десериализация файла {path}")
             transactions_data = json.load(json_data)
+            utils_log.info("Возврат пайтон-данных из json-файла ")
             return transactions_data
     except FileNotFoundError:
+        utils_log.error(f"Возникла ошибка. {path} не найден")
         print("Файл не найден")
         return [{}]
     except json.JSONDecodeError:
+        utils_log.error(f"Возникла ошибка. Формат данных {path} неверный")
         print("Invalid JSON data.")
         return [{}]
